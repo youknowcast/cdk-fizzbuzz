bead336b2b49b659ba4ede1dce26e8c90741179b
youknowcast <ykc@daycrift.net>
2021-03-14 00:32:43 +0900

    end hit counter

--- lib/cdk-fizzbuzz-stack.ts
+++ lib/cdk-fizzbuzz-stack.ts
@@ -1,18 +1,26 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda'
+import * as apigw from '@aws-cdk/aws-apigateway'
+import { HitCounter } from './hitcounter'
 
 export class CdkFizzbuzzStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkFizzbuzzQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'hello.handler'
+    })
+
+    const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello
     });
 
-    const topic = new sns.Topic(this, 'CdkFizzbuzzTopic');
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+//      handler: hello
+      handler: helloWithCounter.handler
+    })
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
